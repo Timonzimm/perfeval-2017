2fb8f80a349c2b13fc63fdad53c18bcde88b171a
JimZer <jimi.vaubien@protonmail.com>
2017-03-06 14:22:58 +0100

    final version of code, only plots title missing

--- HW1/req2.py
+++ HW1/req2.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from matplotlib.artist import setp
 import copy
 from sklearn.linear_model import LinearRegression
+from statsmodels.tsa.arima_model import ARMA 
+import matplotlib.ticker as plticker
 
 parser = AdvancedHTMLParser.AdvancedHTMLParser()
 c = pycurl.Curl()
@@ -58,6 +60,28 @@ def diffReq(nbReq, param, field, step):
         results.append(query(param))
     return np.asarray(results)
 
+def getLinearParam(c):
+    inters = [(1,55), (55,115), (115,175), (175, 235), (235,305), (305,355), (355,425), (425,475), (475,545), (545,595), (595,10000)]
+    params = [(1,1), (2,1), (3,1), (4,1), (5,1), (6,2), (7,2), (8,2), (9,2), (10,2), (10,3)]
+
+    for i in range(11):
+        if (c >= inters[i][0] and c <= inters[i][1]):
+            return params[i]
+    return (1,1)
+
+
+def linearReq(nbReq, param, field, step):
+    results = []
+    p = list(range(0, nbReq*step, step))
+    p[0] = 1
+    for i in range(nbReq):
+        param[field] = p[i]
+        a, s =getLinearParam(i)
+        param['apoints'] = a
+        param['servers'] = s
+        results.append(query(param))
+    return np.asarray(results)
+
 def mean(nbMean, nbReq, param, field, step):
     results = []
     for i in range(nbMean):
@@ -65,6 +89,17 @@ def mean(nbMean, nbReq, param, field, step):
         print(i)
     return np.asarray(results)
 
+def meanLinear(nbMean, nbReq, param, field, step):
+    results = []
+    for i in range(nbMean):
+        results.append(linearReq(nbReq, param, field, step))
+        print(i)
+    return np.asarray(results)    
+
+def movingaverage(data, window_size):
+    window = np.ones(int(window_size))/float(window_size)
+    res = np.apply_along_axis(lambda m: np.convolve(m, window, 'same'), axis=0, arr=data)
+    return res
 
 
 def fetchDataQ1():
@@ -94,12 +129,43 @@ def fetchDataQ3():
 
     np.save('./Q3/data', res)
 
-def plotSeries(data, labels, path, x, y, title='Test'):
+def fetchDataQ4():
+    res = []
+    print(np.shape(res))
+    for a in range (10):
+        for s in range(10):
+            print(a,s)
+            p = copy.deepcopy(paramBase)
+            p['apoints'] = a + 1
+            p['servers'] = s + 1
+            res.append(diffReq(1001, p, 'clients', 1))
+    np.save('./Q4/data', np.asarray(res))
+
+def fetchDataLinear():
+    res = meanLinear(10, 1001, paramBase, 'clients', 1)
+    np.save('./Q4/linear', res)
+
+
+
+def plotSeries(data, labels, path, x, y, title='Test', vline=None):
+    loc = plticker.MultipleLocator(base=200.0)
     df = pd.DataFrame(data, columns=labels)
     ax = df.plot(lw=0.5,colormap='jet',marker='.',markersize=0,title=title)
     ax.set_xlabel(x)
     ax.set_ylabel(y)
     ax.grid()
+    ax.xaxis.set_major_locator(loc)
+    if (vline):
+        plt.axvline(x=vline)
+        plt.text(vline, 0, '%d clients' % vline, rotation=90, verticalalignment='bottom')
+        ax.legend(loc='upper left', labelspacing=0)
+    '''for tick in ax.xaxis.get_major_ticks():
+        tick.label.set_fontsize(3) 
+    for label in ax.xaxis.get_ticklabels()[::1]:
+        label.set_visible(False)
+    for label in ax.xaxis.get_ticklabels()[::5]:
+        label.set_visible(True)'''
+
     plt.savefig(path, format='svg', dpi=800)
 
 def plotSwag(mean, std, labels, path, x, y, title='Test'):
@@ -154,4 +220,34 @@ def plotQ3():
     plotSeries(np.asarray(cp).transpose(), ['1','2','4','8'], './Q3/{0}.svg'.format(columns[2]), x='Number of clients', y=columns[2])
     plotSeries(np.asarray(d).transpose(), ['1','2','4','8'], './Q3/{0}.svg'.format(columns[3]), x='Number of clients', y=columns[3])
 
-plotQ2()
+def plotQ4():
+    res = np.load('./Q4/data.npy')
+    for a in range(100):
+        p = res[a]
+        plotSeries(p.transpose()[0], [columns[0]], 
+        './Q4/S{0}A{1}.svg'.format(a%10 + 1,a/10 + 1), x='Number of clients', y=columns[0])
+
+def plotQ4alt():
+    res = np.load('./Q4/data.npy')
+    res = res.transpose((2,1,0))
+    res = res[0]
+    vline = [55, 115, 175, 235, 305, 355, 425, 475, 545, 595]
+    print(res.shape)
+    for i in range(10):
+        labs = list(range(1,11,1))
+        labs = ['{0}AP,{1}S '.format(i+1, x) for x in labs]
+        print(res[:, i*10:i*10 + 10].shape)
+        p = res[:, i*10:i*10 + 10]
+        plotSeries(p, labs, 
+        './Q4/test{0}.svg'.format(i), x='Number of clients', y=columns[0], vline=vline[i])
+
+def plotLinear():
+    res = np.load('./Q4/linear.npy')
+    mean = res.mean(0).transpose()
+    std = res.std(0).transpose()
+    for i in range(4):
+        plotSwag(mean[i], std[i], [columns[i]], 
+        './Q4/{0}.svg'.format(columns[i]), x='Number of clients', y=columns[i])
+
+
+
