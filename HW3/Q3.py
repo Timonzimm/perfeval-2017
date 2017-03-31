ca0bf0623ed4fbb2289e0dd22ae392e8fead4b85
JimZer <jimi.vaubien@protonmail.com>
2017-03-31 19:13:50 +0200

    computing the ci done, need to remove transcient now

--- HW3/Q3.py
+++ HW3/Q3.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from simulator import Simulator
+from math import *
 
 
 def ci_lambda(l, n):
@@ -28,10 +29,41 @@ def ci_lambda(l, n):
 
     m = np.mean(tests, 0)
 
-    np.savetxt('n50_l70', m)
-    np.savetxt('n50_l70_samples', samples)
+    np.savetxt('n50_l108', m)
+    np.savetxt('n50_l108_samples', samples)
 
-ci_lambda(70, 50)
+
+def ci_med(serie):
+    n = serie.size
+    j = floor( 0.50*n - 0.980*sqrt(n))
+    k = ceil(0.50*n + 0.980*sqrt(n) + 1)
+    return (serie[j],  serie[k])
+
+def ci_mean(serie):
+    n = serie.size
+    m = serie.mean()
+    s = np.std(serie)
+    return (m - 1.96*s/sqrt(n), m + 1.96*s/sqrt(n))
+
+    
+
+
+def ci():
+    l70 = np.loadtxt('n50_l70.gz')
+    l108 = np.loadtxt('n50_l108.gz')
+
+    ci70_med = ci_med(l70)
+    ci70_mean = ci_mean(l70)
+    ci108_med = ci_med(l108)
+    ci108_mean = ci_mean(l108)
+
+    print('Confidence inverval for the mediane at lambda = 70: {}', ci70_med)
+    print('Confidence inverval for the mean at lambda = 70: {}', ci70_mean)
+    print('Confidence inverval for the mediane at lambda = 108: {}', ci108_med)
+    print('Confidence inverval for the mediane at lambda = 108: {}', ci108_mean)
+
+ci()
+
 
 
 
