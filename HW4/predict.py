476bf827cd8f75ad10378b7b61626f869da80e32
Timon <timonzimm@gmail.com>
2017-04-14 11:06:56 +0200

    .

--- HW4/predict.py
+++ HW4/predict.py
@@ -1,4 +1,11 @@
+import os
+import time
+import warnings
 import numpy as np
+from numpy import newaxis
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
 
 def load_data(filename, seq_len, normalise_window):
   f = open(filename, 'rb').read()
@@ -24,3 +31,25 @@ def load_data(filename, seq_len, normalise_window):
 
   return [x_train, y_train, x_test, y_test]
 
+def build_model(layers):
+  model = Sequential()
+
+  model.add(LSTM(
+      input_dim=layers[0],
+      output_dim=layers[1],
+      return_sequences=True))
+  model.add(Dropout(0.2))
+
+  model.add(LSTM(
+      layers[2],
+      return_sequences=False))
+  model.add(Dropout(0.2))
+
+  model.add(Dense(
+      output_dim=layers[3]))
+  model.add(Activation("linear"))
+
+  start = time.time()
+  model.compile(loss="mse", optimizer="rmsprop")
+  print("> Compilation Time : ", time.time() - start)
+  return model
