e01ebb4953c0250d5aacb882dfa18c50e38e2684
Timon <timonzimm@gmail.com>
2017-04-14 11:14:34 +0200

    .

--- HW4/run.py
+++ HW4/run.py
@@ -1,21 +1,50 @@
 import lstm
+import time
+import matplotlib.pyplot as plt
 
-epochs  = 1
-seq_len = 50
+def plot_results(predicted_data, true_data):
+  fig = plt.figure(facecolor='white')
+  ax = fig.add_subplot(111)
+  ax.plot(true_data, label='True Data')
+  plt.plot(predicted_data, label='Prediction')
+  plt.legend()
+  plt.show()
 
-print('> Loading data... ')
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+  fig = plt.figure(facecolor='white')
+  ax = fig.add_subplot(111)
+  ax.plot(true_data, label='True Data')
+  #Pad the list of predictions to shift it in the graph to it's correct start
+  for i, data in enumerate(predicted_data):
+      padding = [None for p in range(i * prediction_len)]
+      plt.plot(padding + data, label='Prediction')
+      plt.legend()
+  plt.show()
 
-X_train, y_train, X_test, y_test = lstm.load_data('sinwave.csv', seq_len, True)
+#Main Run Thread
+if __name__=='__main__':
+	global_start_time = time.time()
+	epochs  = 1
+	seq_len = 50
 
-print('> Data Loaded. Compiling...')
+	print('> Loading data... ')
 
-model = lstm.build_model([1, 50, 100, 1])
+	X_train, y_train, X_test, y_test = lstm.load_data('sinwave.csv', seq_len, True)
 
-model.fit(
-    X_train,
-    y_train,
-    batch_size=512,
-    nb_epoch=epochs,
-    validation_split=0.05)
+	print('> Data Loaded. Compiling...')
 
-predicted = lstm.predict_point_by_point(model, X_test)
+	model = lstm.build_model([1, 50, 100, 1])
+
+	model.fit(
+	    X_train,
+	    y_train,
+	    batch_size=512,
+	    nb_epoch=epochs,
+	    validation_split=0.05)
+
+	predictions = lstm.predict_sequences_multiple(model, X_test, seq_len, 50)
+	#predicted = lstm.predict_sequence_full(model, X_test, seq_len)
+	#predicted = lstm.predict_point_by_point(model, X_test)        
+
+	print('Training duration (s) : ', time.time() - global_start_time)
+	plot_results_multiple(predictions, y_test, 50)
