f56ac300e25454892e0c403d7e12f3d279618565
Timon <timonzimm@gmail.com>
2017-04-14 11:09:56 +0200

    .

--- HW4/predict.py
+++ HW4/predict.py
@@ -56,3 +56,23 @@ def build_model(layers):
   model.compile(loss="mse", optimizer="rmsprop")
   print("> Compilation Time : ", time.time() - start)
   return model
+
+epochs  = 1
+seq_len = 50
+
+print('> Loading data... ')
+
+X_train, y_train, X_test, y_test = lstm.load_data('sp500.csv', seq_len, True)
+
+print('> Data Loaded. Compiling...')
+
+model = lstm.build_model([1, 50, 100, 1])
+
+model.fit(
+    X_train,
+    y_train,
+    batch_size=512,
+    nb_epoch=epochs,
+    validation_split=0.05)
+
+predicted = lstm.predict_point_by_point(model, X_test)
